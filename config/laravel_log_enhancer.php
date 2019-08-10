d1a64011ceed036d9420b1e88ecd7aec1c81b95f
Dan Stuchbury <dan.stuchbury@icloud.com>
2019-08-10 11:36:27 +0100

    Set up config options

---
+++ config/laravel_log_enhancer.php
@@ -0,0 +1,18 @@
+<? php
+
+return [
+    'log_request_details' => true,
+
+    'log_input_details' => true,
+
+    'log_request_headers' => true,
+
+    'log_session_data' => true,
+
+    'log_memory_usage' => false,
+
+    'log_git_data' => false,
+
+    // Specify the inputs from the user that should be ignored
+    'ignore_input_fields => ['password', 'confirm_password'],
+];
