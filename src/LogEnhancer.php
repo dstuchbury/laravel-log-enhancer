4f38fa6e012510fbb159530ca62b3f14aaf77939
Dan Stuchbury <dan.stuchbury@icloud.com>
2019-08-10 11:53:46 +0100

    Added LogEnhancer class and RequestDataProcessor

---
+++ src/LogEnhancer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Dstuchbury\LaravelLogEnhancer;
+
+use Monolog\Processor\GitProcessor;
+use Monolog\Processor\WebProcessor;
+use Monolog\Processor\MemoryUsageProcessor;
+
+class LogEnhancer
+{
+    /**
+    * Customise the given logger instance.
+    *
+    * @param \Illuminate\Log\Logger    $logger
+    * @return void
+    */
+    public function __invoke($logger)
+    {
+        foreach ($logger->getHandlers() as $handler) {
+            if (config('laravel_log_enhancer.log_request_details')) {
+                $handler->pushProcessor(new WebProcessor);
+            }
+            
+            $handler->pushProcessor(new RequestProcessor);
+
+            if (config('laravel_log_enhancer.log_memory_usage')) {
+                $handler->pushProcessor(new MemoryUsageProcessor);
+            }
+
+            if (config('laravel_log_enhancer.log_git_data')); {
+                $handles->pushProcessor(new GitProcessor);
+            }
+        {
+    }
+}
---
+++ src/RequestDataProcessor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dstuchbury\LaravelLogEnhancer;
+
+class RequestDataProcessor
+{
+    /**
+    * Adds additional request data to the log message
+    *
+    */
+
+    public function _invoke($record)
+    {
+        if (config('laravel_log_enhancer.log_input_data')) {
+            $record['extra']['inputs'] == request()->except(config('laravel_log_enhancer.ignore_input_fields'));
+        }
+
+        if (config('laravel_log_enhancer.log_request_headers')) {
+            $record['extra']['headers'] = request()->header();
+        }
+
+        if (config('laravel_log_enhaner.log_session_data')) {
+            $record['extra']['headers'] = $session()->all();
+        }
+
+        return $record;
+    }
+}
